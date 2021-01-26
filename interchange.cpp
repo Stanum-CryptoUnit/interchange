654cf2525e0dcc129b69f03c00b74114ab8b1b22
core <core@stanum.us>
2021-01-26 12:53:45 +0300

    fix

--- interchange.cpp
+++ interchange.cpp
@@ -5,8 +5,8 @@
 using namespace eosio;
 
 
-[[eosio::action]] void interchange::cancel(eosio::name username, uint64_t id){
-  require_auth(username);
+[[eosio::action]] void interchange::cancel(eosio::name username, uint64_t id, uint64_t est_converted_month){
+  require_auth(_changer);
 
   changes_index changes(_me, _me.value);
   auto change = changes.find(id);
@@ -14,20 +14,44 @@ using namespace eosio;
   eosio::check(change != changes.end(), "Change object is not found");
   eosio::check(change -> status == "process"_n, "Change object is proccessed or canceled");
 
-  action(
-      permission_level{ _me, "active"_n },
-      _token_contract, "transfer"_n,
-      std::make_tuple( _me, change -> username, change -> cru_quantity, std::string("")) 
-  ).send(); 
+  if (change -> is_lock == false) {
+    
+    action(
+        permission_level{ _me, "active"_n },
+        _token_contract, "transfer"_n,
+        std::make_tuple( _me, change -> username, change -> cru_quantity, std::string("")) 
+    ).send(); 
 
+  } else {
 
-  changes.modify(change, _me, [&](auto &c){
-    c.status = "canceled"_n;
-  });  
+    action(
+        permission_level{ _me, "active"_n },
+        _tokenlock, "intcancel"_n,
+        std::make_tuple( change -> username, change -> cru_quantity, change->lock_id, est_converted_month) 
+    ).send();
+
+    action(
+        permission_level{ _me, "active"_n },
+        _token_contract, "transfer"_n,
+        std::make_tuple( _me, _tokenlock, change -> cru_quantity, std::string("")) 
+    ).send(); 
+
+  }
+
+  changes.erase(change);  
 
 }
 
-[[eosio::action]] void interchange::change(uint64_t id){
+[[eosio::action]] void interchange::intchange(eosio::name username, eosio::asset quantity, uint64_t lock_id){
+
+  require_auth(_tokenlock);
+
+  interchange::create_change_object(username, quantity, true, lock_id);
+
+};
+
+
+[[eosio::action]] void interchange::approvech(uint64_t id, eosio::name username, uint64_t lock_id, eosio::asset amount){
     require_auth(_changer);
 
     changes_index changes(_me, _me.value);
@@ -35,32 +59,45 @@ using namespace eosio;
 
     eosio::check(change != changes.end(), "Change object is not found");
     eosio::check(change -> status == "process"_n, "Change object is proccessed or canceled");
+    eosio::check(change -> username == username, "Wrong username for current change");
+    eosio::check(change -> wcru_quantity == amount, "Wrong amount for approve");
 
-    action(
-        permission_level{ _me, "active"_n },
-        _token_contract, "retire"_n,
-        std::make_tuple( _me, change -> cru_quantity, std::string("retire")) 
-    ).send(); 
+    // action(
+    //     permission_level{ _me, "active"_n },
+    //     _token_contract, "retire"_n,
+    //     std::make_tuple( _me, change -> cru_quantity, std::string("retire")) 
+    // ).send(); 
 
-
-    action(
-        permission_level{ _me, "active"_n },
-        _token_contract, "transfer"_n,
-        std::make_tuple( _me, change -> username, change -> wcru_quantity, std::string("")) 
-    ).send(); 
-
-
+    eosio::check(lock_id != 0, "lock_id is should be setted");
+    
     changes.modify(change, _changer, [&](auto &c){
+      c.lock_id = lock_id;
       c.status = "success"_n;
     });
 
+    //TODO method ADD on tokenlock by algorithm 3
+    auto dt = eosio::time_point_sec(eosio::current_time_point().sec_since_epoch());
+
+    action(
+        permission_level{ _tokenlock, "active"_n },
+        _tokenlock, "add"_n,
+        std::make_tuple( change->username, (uint64_t)lock_id, (uint64_t)0, dt, (uint64_t)3, (int64_t)change->wcru_quantity.amount) 
+    ).send(); 
+    
 
   }
 
- void interchange::create_change_object(eosio::name username, eosio::asset quantity){
-    require_auth(username);
-  
+ void interchange::create_change_object(eosio::name username, eosio::asset quantity, bool is_lock, uint64_t lock_id){
+    
     changes_index changes(_me, _me.value);
+    eosio::check(quantity.symbol == _cru_symbol, "Wrong symbol for change");
+
+    rate_index rates(_me, _me.value);
+    auto rate = rates.find(0);
+    double exchange_rate = 1;
+
+    if (rate != rates.end())
+      exchange_rate = rate -> exchange_rate;
 
     changes.emplace(_me, [&](auto &r){
       r.id = changes.available_primary_key();
@@ -69,6 +106,9 @@ using namespace eosio;
       r.cru_quantity = quantity;
       r.wcru_quantity = eosio::asset(quantity.amount, _wcru_symbol);
       r.status = "process"_n;
+      r.is_lock = is_lock;
+      r.lock_id = lock_id;
+      r.exchange_rate = exchange_rate;
     });
     
   }
@@ -79,11 +119,13 @@ extern "C" {
    /// The apply method implements the dispatch of events to this contract
    void apply( uint64_t receiver, uint64_t code, uint64_t action ) {
         if (code == interchange::_me.value) {
-          if (action == "change"_n.value){
-            execute_action(name(receiver), name(code), &interchange::change);
+          if (action == "approvech"_n.value){
+            execute_action(name(receiver), name(code), &interchange::approvech);
+          }  else if (action == "intchange"_n.value){
+            execute_action(name(receiver), name(code), &interchange::intchange);
           } else if (action == "cancel"_n.value){
             execute_action(name(receiver), name(code), &interchange::cancel);
-          }             
+          }               
         } else {
 
           if (action == "transfer"_n.value){
@@ -98,11 +140,12 @@ extern "C" {
             auto op = eosio::unpack_action_data<transfer>();
 
             if (op.to == interchange::_me){
-                if (op.from != interchange::_reserve){
+                if (op.from != interchange::_worldcru && op.from != interchange::_tokenlock){
+                  require_auth(op.from);
                   eosio::check(name(code) == interchange::_token_contract, "Wrong token contract");
                   eosio::check(op.quantity.symbol == interchange::_cru_symbol, "Wrong symbol");
 
-                  interchange::create_change_object(op.from, op.quantity);  
+                  interchange::create_change_object(op.from, op.quantity, false, 0);  
                 }
                 
             }
--- interchange.hpp
+++ interchange.hpp
@@ -11,25 +11,29 @@ class [[eosio::contract]] interchange : public eosio::contract {
 public:
     interchange( eosio::name receiver, eosio::name code, eosio::datastream<const char*> ds ): eosio::contract(receiver, code, ds)
     {}
-    [[eosio::action]] void change(uint64_t id);
-    [[eosio::action]] void cancel(eosio::name username, uint64_t id);
-    
-    static void create_change_object(eosio::name username, eosio::asset quantity);
+    [[eosio::action]] void approvech(uint64_t id, eosio::name username, uint64_t lock_id, eosio::asset amount);
+    [[eosio::action]] void cancel(eosio::name username, uint64_t id, uint64_t est_converted_month);
+    [[eosio::action]] void intchange(eosio::name username, eosio::asset quantity, uint64_t lock_id);
+
+    static void create_change_object(eosio::name username, eosio::asset quantity, bool is_lock, uint64_t lock_id);
 
 
     static constexpr eosio::name _me = "interchange"_n; 
     static constexpr eosio::name _changer = "changer"_n;   
-    static constexpr eosio::name _reserve = "reserve"_n;   
+    static constexpr eosio::name _worldcru = "worldcru"_n;   
+    static constexpr eosio::name _tokenlock = "tokenlock"_n;   
 
     static constexpr eosio::name _token_contract = "eosio.token"_n;
-    static constexpr eosio::symbol _cru_symbol     = eosio::symbol(eosio::symbol_code("CRU"), 0);
-    static constexpr eosio::symbol _wcru_symbol     = eosio::symbol(eosio::symbol_code("WCRU"), 0);
+    static constexpr eosio::symbol _cru_symbol     = eosio::symbol(eosio::symbol_code("CRU"), 4);
+    static constexpr eosio::symbol _wcru_symbol     = eosio::symbol(eosio::symbol_code("WCRU"), 4);
     
     
     struct [[eosio::table]] changes {
         uint64_t id;
         eosio::time_point_sec date;
-
+        bool is_lock;
+        uint64_t lock_id;
+        double exchange_rate;
         eosio::name username;
         eosio::asset cru_quantity;
         eosio::asset wcru_quantity;
@@ -42,7 +46,7 @@ public:
 
         
 
-        EOSLIB_SERIALIZE(changes, (id)(date)(username)(cru_quantity)(wcru_quantity)(status))
+        EOSLIB_SERIALIZE(changes, (id)(date)(is_lock)(lock_id)(exchange_rate)(username)(cru_quantity)(wcru_quantity)(status))
     };
 
     typedef eosio::multi_index<"changes"_n, changes,
@@ -52,6 +56,19 @@ public:
     
     > changes_index;
 
+
+    struct [[eosio::table]] rate {
+        uint64_t id;
+        double exchange_rate = 1;
+        
+        uint64_t primary_key() const {return id;}
+        
+        
+
+        EOSLIB_SERIALIZE(rate, (id)(exchange_rate))
+    };
+
+    typedef eosio::multi_index<"rate"_n, rate    > rate_index;
 
 
 
